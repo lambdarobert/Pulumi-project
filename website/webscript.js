c83520eace1458f540c8bee1a6e6bee16551dd8b
WackyWillard <WackyWillard@github.com>
2023-06-01 20:11:54 -0700

    Added hcaptcha element retrival

--- website/webscript.js
+++ website/webscript.js
@@ -4,41 +4,43 @@ document.getElementById('startServerBtn').addEventListener('click', startServer)
 window.onload = getStatus;
 
 function startServer() {
-  // Retrieve the hcaptcha token
-  const hcaptchaToken = ''; // Add your code here to obtain the hcaptcha token
+	// Retrieve the hcaptcha token
+	const hcaptchaDiv = document.getElementById('h-captcha')
+	const iframe = hcaptchaDiv.querySelector('iframe')
+	const hcaptchaToken = iframe.getAttribute('data-hcaptcha-response')
 
-  // Make the POST request to start the server
-  fetch('https://api.robert.zip/server/start', {
-    method: 'POST',
-    body: new URLSearchParams({
-      'hcaptcha_token': hcaptchaToken
-    })
-  })
-  .then(handleResponse)
-  .then(displayResponse)
-  .catch(displayError);
+	// Make the POST request to start the server
+	fetch('https://api.robert.zip/server/start', {
+		method: 'POST',
+		body: new URLSearchParams({
+			'hcaptcha_token': hcaptchaToken
+		})
+	})
+		.then(handleResponse)
+		.then(displayResponse)
+		.catch(displayError);
 }
 
 function getStatus() {
-  // Make the GET request to get the server status
-  fetch('https://api.robert.zip/server/status')
-    .then(handleResponse)
-    .then(displayResponse)
-    .catch(displayError);
+	// Make the GET request to get the server status
+	fetch('https://api.robert.zip/server/status')
+		.then(handleResponse)
+		.then(displayResponse)
+		.catch(displayError);
 }
 
 function handleResponse(response) {
-  if (!response.ok) {
-    throw new Error(`Request failed with status ${response.status}`);
-  }
-  return response.text();
+	if (!response.ok) {
+		throw new Error(`Request failed with status ${response.status}`);
+	}
+	return response.text();
 }
 
 function displayResponse(responseText) {
-  document.getElementById('response').textContent = responseText;
+	document.getElementById('response').textContent = responseText;
 }
 
 function displayError(error) {
-  document.getElementById('response').textContent = `An error occurred: ${error.message}`;
+	document.getElementById('response').textContent = `An error occurred: ${error.message}`;
 }
 
